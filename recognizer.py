a53a0a23a7338ab8b0670270acead69891e26f80
Davi-Ga <davigaldinoky@gmail.com>
2023-02-28 15:50:23 -0300

    PT-BR

--- recognizer.py
+++ recognizer.py
@@ -3,44 +3,49 @@ from queue import Queue
 from threading import Thread
 from random import choice
 from speaker import sound_out
-language = 'pt-BR'
+from config import conversations,commands
 
 rec = sr.Recognizer()
 audio_queue = Queue()
 def recognize_worker():
     errors_list = [
-    "MARI could not understand audio",
-    "MARI asked you to repeat",
-    "Sorry, MARI could not understand what you said",
+        "MARI não conseguiu te ouvir",
+        "MARI pediu para você repetir",
+        "Desculpe, MARI não entendeu o que você disse",
+        "MARI não entendeu o que você disse, por favor repita",
     ]
-    
     while True:
         audio=audio_queue.get()
         if audio is None:
             break
         try:
-            inpt=rec.recognize_google(audio,language=language,show_all=False,with_confidence=True)
+            inpt=rec.recognize_google(audio,language="pt-BR")
             audio_queue.task_done()
             return "{}".format(inpt)
-            #print("MARI thinks you said: " + rec.recognize_google(audio,language=language))
         
         except sr.UnknownValueError:
             return choice(errors_list)
         except sr.RequestError as e:
-            return ("Could not request results from MARI service;- {0}").format(e)
+            return ("Não conseguimos resultados com o serviço da MARI;- {0}").format(e)
 
 recognize_thread=Thread(target=recognize_worker)
 recognize_thread.daemon = True
 recognize_thread.start()
 
+print("MARI está ouvindo...")
 with sr.Microphone() as source:
     rec.adjust_for_ambient_noise(source)
     try:    
-        print("MARI is listening...")
         while True:
             audio_queue.put(rec.listen(source))
             response=recognize_worker()
-            sound_out(response)
+            print("Você disse: {}".format(response))
+            
+            mari_response = commands[response]
+            sound_out(mari_response)
+            print("MARI: {}".format(mari_response))
+            
+            
     except KeyboardInterrupt:
         pass    
 
