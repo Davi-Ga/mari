c1bc2bcb2faa1beab0d6be59d261d1ae0408b6a8
Davi-Ga <davigaldinoky@gmail.com>
2023-02-28 15:01:22 -0300

    new speaker

--- speaker.py
+++ speaker.py
@@ -4,4 +4,11 @@ engine=pyttsx3.init()
 
 def sound_out(output):
     engine.say(output)
+    
+    voices=engine.getProperty('voices')
+    volume=engine.getProperty('volume')
+    
+    engine.setProperty(volume,1)
+    engine.setProperty('rate', 125) 
+    engine.setProperty('voice', voices[1].id)
     engine.runAndWait()
