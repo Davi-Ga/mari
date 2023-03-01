6e6be142c870dc52622047ef82a4a0f4f02063c4
Davi-Ga <davigaldinoky@gmail.com>
2023-02-28 21:02:06 -0300

    errors list inclued

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
-conversations = {
-	
-}
+errors_list = [
+	"MARI não conseguiu te ouvir",
+	"MARI pediu para você repetir",
+	"Desculpe, MARI não entendeu o que você disse",
+	"MARI não entendeu o que você disse, por favor repita",
+]
+    
 
 commands = {
 	'desligar': 'desligando',
